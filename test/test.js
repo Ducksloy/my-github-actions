a4ead6b6de23c6143aed43782b00b81d318a4b27
Pidun Phum <pidun.phum@student.cadt.edu.kh>
2025-07-09 23:31:32 +0700

    Move test.js into test folder for Mocha

---
+++ test/test.js
@@ -0,0 +1,20 @@
+// test/test.js
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../app/app'); // Adjust path if needed
+
+const assert = chai.assert;
+chai.use(chaiHttp);
+
+describe('GET /', function () {
+  it('should return hello message', function (done) {
+    chai.request(app)
+      .get('/')
+      .end((err, res) => {
+        assert.equal(res.text, 'Hello, GitHub Actions!');
+        done();
+      });
+  });
+});
+
